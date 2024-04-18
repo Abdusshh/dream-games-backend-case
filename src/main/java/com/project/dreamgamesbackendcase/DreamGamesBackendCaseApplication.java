14612630b94e035db21db766f3dcc9fb85b726a1
Abdusshh <abdullah.gules@simpplr.com>
2024-04-18 11:22:42 +0300

    Initial commit

---
+++ src/main/java/com/project/dreamgamesbackendcase/DreamGamesBackendCaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.dreamgamesbackendcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamGamesBackendCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamGamesBackendCaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/dreamgamesbackendcase/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.project.dreamgamesbackendcase.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+}
